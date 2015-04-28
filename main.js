792d6692cf00e2a3fe2dab4f0318a63c4edf5a53
Alex Moore <truckfondue@gmail.com>
2015-04-28 17:04:42 -0600

    rendering plans to the dom

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ $(document).on('ready', function() {
   
 });
 
+$('#play').on('click', function(){
+	console.log(room1);
+	// $('.playground').after(room1);
+});
+
 // A game will be an instance of the World COnstructor
 
 var World = function(name, player, rooms) {
@@ -22,12 +27,14 @@ var Player = function(name, inventory, strength){
 	this.health = 100; //default value of 100
 	this.strength = strength;
 	// Inventory will be linked to an array of items created via the Item constructor
+	// fight methodMons
 };
 
-var Room = function(monsters, items) {
+var Room = function(plan) {
 	// this.navigationInformation = navigationInformation;
-	this.monsters = monsters;
-	this.items = items;
+	// this.monsters = monsters;
+	// this.items = items; //Add items to the plan rather than as an argument to Room
+	this.plan = plan;
 	// prototype for placement of monsters and items
 };
 
@@ -53,7 +60,7 @@ var Monster = function(name, health, strength){
 var dungeonCrawler = new World('Dungeon Crawler', mike, room1);
 
 //Room
-var room1 = new Room([snarlax, ruby], [largeHealth, steroids]);
+var room1 = new Room([plan1, plan2, plan3, plan4]);
 
 // Player
 
@@ -69,3 +76,99 @@ var steroids = new Item('Steroids', 1000, 'Can you say the HULK?');
 // 
 var ruby = new Monster ('Ruby', 75, 10);
 var snarlax = new Monster ('Snarlax', 50, 5);
+
+
+// o represent monsters
+// # represent borders
+// i represents enemies
+// The container will hold the location of items and monsters
+
+var plan1 =
+  ["############################",
+   "#      #    #             ##",
+   "#                   x      #",
+   "#          #####           #",
+   "##   i     #   #    ##     #",
+   "###           ##     #     #",
+   "#           ###      #     #",
+   "#   ####                   #",
+   "#   ##        %            #",
+   "#    #                 ### #",
+   "#    #                     #",
+   "############################"];
+
+var plan2 =
+  ["############################",
+   "#                     ######",
+   "#    ***                **##",
+   "#   *##**         **  x  *##",
+   "#    ***     i    ##**    *#",
+   "#                 ##***   *#",
+   "#                 ##**    *#",
+   "#   i       #*            *#",
+   "#*          #**           *#",
+   "#***        ##**    x    **#",
+   "#*****     ###***       *###",
+   "############################"];
+
+var plan3 =
+  ["############################",
+   "#                      #####",
+   "#    ##                 ####",
+   "#   ####                  ##",
+   "#    ##       i            #",
+   "#                          #",
+   "#                ###       #",
+   "#               #####      #",
+   "#                ###       #",
+   "# %        ###        x    #",
+   "#        #######           #",
+   "############################"];
+
+
+var plan4 =
+  ["############################",
+   "#                      #####",
+   "#    ##                 ####",
+   "#   ####     i            ##",
+   "#    ##                    #",
+   "#                          #",
+   "#      i         ###       #",
+   "#            %   #####     #",
+   "#                ###       #",
+   "# x        ###        x    #",
+   "#        #######           #",
+   "############################"];
+
+  var Point = function(x, y){
+  		this.x = x;
+  		this.y = y;
+  };
+
+  Point.prototype.add = function(other){
+  	return new Point(this.x = other.x, this.y + other.y);
+  };
+
+  Point.prototype.isEqualTo = function(other) {
+  	return ( this.x === other.x && this.y === other.y );
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
